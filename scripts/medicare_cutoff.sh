7a9fd63b65a3c7e6eea7d00535c4694f1021ba7f
mauriciogtec <mauriciogtec@hsph.harvard.edu>
2023-01-25 00:24:08 -0500

    cutoff script

---
+++ scripts/medicare_cutoff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+num_seeds=50
+
+for (( i=0; i<$(num_seeds); i++))
+do
+    python main_tres_medicare.py --seed=$i --tr_reg --ratio c_ratio --rdir=results/medicare --edir/cutoff &
+done 
