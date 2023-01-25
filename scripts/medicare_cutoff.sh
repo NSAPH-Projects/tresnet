daa6678bf7b843a451be76719055e7059c91da80
mauriciogtec <mauriciogtec@hsph.harvard.edu>
2023-01-25 10:01:38 -0500

    cutoff value

--- scripts/medicare_cutoff.sh
+++ scripts/medicare_cutoff.sh
@@ -4,5 +4,5 @@ num_seeds=50
 
 for (( i=0; i<$((num_seeds)); i++))
 do
-    python main_tres_medicare.py --seed=$((i + num_seeds)) --tr_reg --ratio c_ratio --rdir=results --edir cutoff --eval_every 1 --n_epochs 20
+    python main_tres_medicare.py --seed=$i --tr_reg --ratio c_ratio --rdir=results --edir cutoff --eval_every 1 --n_epochs 20
 done 
